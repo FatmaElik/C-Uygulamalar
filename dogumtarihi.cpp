fbc4d19452f0387ea52ce47533a8fa0b752c83c4
FatmaElik <fatma_elik@hotmail.com>
2020-04-27 16:20:18 +0300

    Add files via upload

---
+++ dogumtarihi.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int bugun, dogumtarihi, yasiniz;
+	printf("bugunun tarihini giriniz");
+	scanf("%d",&bugun);
+	printf("dogum tarihinizi giriniz");
+	scanf("%d",&dogumtarihi);
+	yasiniz=bugun-dogumtarihi;
+	printf("yasiniz:%d",yasiniz);
+
+	
+}
--- sayı_okuma.cpp
+++ sayı_okuma.cpp
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include<conio.h>
 	int main() {
-	int a=10; 
-	int b=3, c=4;
-
-	int x;
-	printf ("\n a:%d \n b:%d \n c:%d",a,b,c);
-	printf(" Lutfen bir sayi giriniz");				/* xi klavyeden girilen deðiþkeni okumak için kullanýrýz*/
-	scanf("%d", &x);
-	printf("girilen sayi:%d", x);  					/*xin içindekini al, %d olan yere koy*/
-	float pi=3.14;
-	printf("8.4f" ,pi);
-	
-	}
-	
+	int a;
+	printf("sayý gir");
+	scanf("%d",&a)
+}
