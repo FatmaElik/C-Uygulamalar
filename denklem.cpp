ae147e3d0db15e9d95c2cf21b32ffdccd22da972
FatmaElik <fatma_elik@hotmail.com>
2020-04-27 16:41:44 +0300

    Add files via upload
    Calculate equation f=a^2+b^2+3*c

---
+++ denklem.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int a,b,c;
+	printf("bir sayi girin");
+	scanf("%d",&a);
+	printf("bir sayi girin");
+	scanf("%d",&b);
+	printf("bir sayi girin");
+	scanf("%d",&c);
+	printf("sonuc:%d",a*a+b*b+3*c);
+}
